be88a2f1504af14cac67dc9b59720761164bdcd6
Vintter <88837601+Vintter@users.noreply.github.com>
2021-08-16 15:06:24 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,9 +17,9 @@ using std::reverse;
 int main() {
 
     set a,b;
-    map mutation;
+    map geneA,geneB;
     uint_fast32_t A,B;
-    string x,y,z,w;
+    string x,y,z,w,k;
     vector<string> p(2);
 
 #ifdef LOCAL
@@ -28,41 +28,65 @@ int main() {
 
     cin>>A>>B;
 
-    for(uint_fast16_t i=A;i>0;i--){
+    for(int i=A;i>0;i--){
         cin>>x;
         a.insert(set::value_type(x));
     }
 
-    for(uint_fast16_t i=B;i>0;i--){
+    for(int i=B;i>0;i--){
         cin>>x;
-        b.insert(set::value_type(x));
+        string y (x.rbegin(), x.rend());
+        b.insert(set::value_type(y));
     }
 
-   for(set::const_iterator it = a.begin();it != a.end(); ++it){
-       x = *it;
-       for(int i=0; i < x.length()-1; i++){
-           y+=x[i];
-           if (a.count(y) > 0) {
-               for (int j=i+1; j<x.length(); j++){
-                    z += x[j];
-               }
-               for(set::const_iterator it2 = b.begin();it2 != b.end(); ++it2){
-                   w = z + *it2;
-                   if (b.count(w) > 0) {
-                       a.erase(x);
-                       a.erase(y);
-                       b.erase(*it2);
-                       b.erase(w);
-                   }
-               }
-           }
-       }
-   }
+
+    set::const_iterator it1 = a.begin();
+    set::const_iterator it2 = b.begin();
+    while(it1 != a.end() && it2 != b.end()){
+        x = *it1;
+        w = *it2;
+        for(int i=0; i < x.length()-1 || i < w.length()-1 ; i++) {
+            y += x[i];
+            z += w[i];
+            if(x.size()-1 > i && a.contains(y)){
+                p[0] = x;
+                p[1] = y;
+                for (int j=i+1; j<x.length(); j++){
+                    k += x[j];
+                }
+                geneA.insert(map::value_type(k,p));
+            }
+            k = "";
+            if(w.size()-1 > i && b.contains(z)){
+                p[0] = w;
+                p[1] = z;
+                for (int j=i+1; j<w.length(); j++){
+                    k += w[j];
+                }
+                geneB.insert(map::value_type(k,p));
+            }
+        }
+        y = "";
+        z = "";
+        it1++;
+        it2++;
+    }
+
+    for(map::iterator i1 = geneA.begin();i1 != geneA.end(); ++i1){
+        if(geneB.count(i1->first) > 0){
+            map::iterator i2 = geneB.find(i1->first);
+            a.erase(i1->second[0]);
+            a.erase(i1->second[1]);
+            b.erase(i2->second[0]);
+            b.erase(i2->second[1]);
+        }
+    }
+
     cout<<a.size()<<' '<<b.size()<<endl;
 
 #ifdef LOCAL
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds >(end - start).count() << std::endl;
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds >(end - start).count() << std::endl;
 #endif
     return 0;
 }
